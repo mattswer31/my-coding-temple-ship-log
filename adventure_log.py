f4a3ac859e3129c0efaaa5f016858099640bbc7c
mattswer31 <songma418@gmail.com>
2024-08-07 15:46:23 -0400

    conflict 4

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print ("ahoy coding templers")
+    print ("ahoy coding templers. hello hello hello conflict")
 
 if __name__ == "__main__":
     greet_coding_temple()
