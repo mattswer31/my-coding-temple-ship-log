5c66a6b20e2e4ffcee372ec4353811c9a1d9eb54
mattswer31 <songma418@gmail.com>
2024-08-07 16:08:01 -0400

    1st python exercise

---
+++ mod2_multiline.py
@@ -0,0 +1,13 @@
+a = '''testing bla blah blah spotify
+apple music tidal
+asdfdawwww'''
+
+b = "hello butts \
+how are you"
+
+print (a)
+print (b)
+
+#this brilliant code adds 1+3 and prints it
+c = 1 + 3
+print (c)
