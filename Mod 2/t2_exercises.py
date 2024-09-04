5f439b7efb8869a395f51e5132086345462da643
mattswer31 <songma418@gmail.com>
2024-09-04 19:25:24 -0400

    m2t2 done

---
+++ Mod 2/t2_exercises.py
@@ -0,0 +1,36 @@
+#1. Naming Convention Challenge: Create a variable that holds your full name.
+full_name = "Matt Song"
+
+#2. case sensitivity quiz: define myName and myname. assign first name to myName and last to myname
+myName = "Matt"
+myname = "Song"
+
+#3. assignment practice: assign color to variable fav_color
+fav_color = "blue"
+print("favorite color is " + fav_color)
+
+#4. multiple variable assignment: assign apple banana cherry to fruit1,2,3
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+
+#5. datatypes quest: assign an int float and string to 3 variables and print using type
+num1 = 2
+num2 = 2.0
+num3 = "2"
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+#6. the variables identity card: for mystery_variable = "1234" use type() to determine type
+mysteryvariable = "1234"
+print(type(mysteryvariable))
+
+#7. dynamic typing challenge assign num to var called dynamicVar then reassign as str
+dynamicVar = 3
+dynamicVar = "stringcheese"
+
+#8. keywords examination: write script that tries to use if as a var
+# if = 123
+# print (if)
+
+#9. constants discovery: define a constant named pi and assign it as 3.14159
+PI = 3.14159
